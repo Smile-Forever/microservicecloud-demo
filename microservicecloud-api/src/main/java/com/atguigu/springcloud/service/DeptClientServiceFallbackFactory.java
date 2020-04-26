65ed43b116154efe8ffa85fc0c06e16e4cb6fab6
Your Name <you@example.com>
2020-04-26 11:04:45 +0800

    SpringCloud学习demo之Feign

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientServiceFallbackFactory.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -9,30 +10,27 @@ import com.atguigu.springcloud.entities.Dept;
 import feign.hystrix.FallbackFactory;
 
 @Component // 不要忘记添加，不要忘记添加
-public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService>
-{
-	@Override
-	public DeptClientService create(Throwable throwable)
-	{
-		return new DeptClientService() {
-			@Override
-			public Dept get(long id)
-			{
-				return new Dept().setDeptno(id).setDname("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭")
-						.setDb_source("no this database in MySQL");
-			}
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService> {
+    @Override
+    public DeptClientService create(Throwable throwable) {
+        return new DeptClientService() {
+            @Override
+            public Dept get(long id) {
+                return new Dept()
+                        .setDeptno(id)
+                        .setDname("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭")
+                        .setDb_source("no this database in MySQL");
+            }
 
-			@Override
-			public List<Dept> list()
-			{
-				return null;
-			}
+            @Override
+            public List<Dept> list() {
+                return null;
+            }
 
-			@Override
-			public boolean add(Dept dept)
-			{
-				return false;
-			}
-		};
-	}
+            @Override
+            public boolean add(Dept dept) {
+                return false;
+            }
+        };
+    }
 }
